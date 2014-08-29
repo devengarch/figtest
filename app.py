b153ac7e8f385523a4814fb64efcfaba80a6f396
Justin Luna <justin@lunamoon.us>
2014-08-29 08:59:10 -0600

    added sup hun to app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ redis = Redis(host="redis_1", port=6379)
 @app.route('/')
 def hello():
     redis.incr('hits')
-    return 'SUP World! I have been seen %s times.' % redis.get('hits')
+    return 'SUP hun! I have been seen %s times.' % redis.get('hits')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
