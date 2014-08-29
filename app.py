d50825017a09e2b78d4288658116c5ef8390c62d
Justin Luna <justin@lunamoon.us>
2014-08-29 09:49:02 -0600

    .2 again.2 again

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ redis = Redis(host="redis_1", port=6379)
 @app.route('/')
 def hello():
     redis.incr('hits')
-    return 'What Up World! I have been seen %s times.' % redis.get('hits')
+    return 'What Up World 2! I have been seen %s times.' % redis.get('hits')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
